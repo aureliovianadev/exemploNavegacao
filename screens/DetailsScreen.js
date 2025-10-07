d445772dd15fad76ea4a06ff6c19f6d79b274f21
aureliovianadev <aureliovianadev@gmail.com>
2025-10-06 22:29:48 -0300

    2° Commit

---
+++ screens/DetailsScreen.js
@@ -0,0 +1,10 @@
+import{View, Text, StyleSheet} from 'react-native'
+
+export default function DetailsScreen(){
+
+    return(
+        <View>
+            <Text>Tela de detalhes</Text>
+        </View>
+    )
+}
